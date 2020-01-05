d84e29b3e49dd8c0815247d4eddab7e59bb907da
Jeff Ferguson <JeffFerguson@msn.com>
2020-01-05 16:45:29 -0600

    A new Gepsio XBRL document is created with every load.

--- XBRLInspector.Blazor/Models/XbrlDocumentCacheModel.cs
+++ XBRLInspector.Blazor/Models/XbrlDocumentCacheModel.cs
@@ -1,16 +1,32 @@
 ﻿using JeffFerguson.Gepsio;
 using System;
+using System.Threading.Tasks;
 
 namespace XBRLInspector.Blazor.Models
 {
+    /// <summary>
+    /// A class to cache the loaded XBRL document as loaded by Gepsio.
+    /// </summary>
+    /// <remarks>
+    /// This class is a hack to share state simply. It is a singleton class that
+    /// keeps track of the loaded XBRL document between pages. In a more sophisticated
+    /// system, this should be replaced with something more robust.
+    /// </remarks>
     public class XbrlDocumentCacheModel
     {
-        private static readonly Lazy<XbrlDocument> lazy = new Lazy<XbrlDocument>(() => new XbrlDocument());
+        private static XbrlDocument doc;
 
-        public static XbrlDocument Instance { get { return lazy.Value; } }
+        public static XbrlDocument Instance { get { return doc; } }
 
         private XbrlDocumentCacheModel()
         {
+            doc = new XbrlDocument();
+        }
+
+        public static async Task LoadAsync(string path)
+        {
+            doc = new XbrlDocument();
+            await doc.LoadAsync(path);
         }
     }
 }
