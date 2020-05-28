a941735ac69b5f6a56510e1e58506ef5a22a22ac
Jeff Ferguson <JeffFerguson@msn.com>
2020-05-27 21:58:02 -0500

    DevExpress Blazor controls available. Contexts now display using DxDataGrid.

--- XBRLInspector.Blazor/Program.cs
+++ XBRLInspector.Blazor/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace XBRLInspector.Blazor
 {
@@ -16,6 +17,9 @@ namespace XBRLInspector.Blazor
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            //var builder = CreateHostBuilder(args);
+            //builder.Services.AddDevExpressBlazor();
+            //await builder.Build().RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
